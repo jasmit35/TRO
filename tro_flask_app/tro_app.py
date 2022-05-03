1718121fe11872700c2ce2897f0d0d6195ee58ae
Jeff Smith <js8335@swbell.net>
2022-05-03 14:31:28 -0500

    Tweaks to switch to an html interface.

--- tro_flask_app/tro_app.py
+++ tro_flask_app/tro_app.py
@@ -1,16 +1,39 @@
+#
 from flask import Flask
+from flask import request
 
 server = Flask(__name__)
 
 
-@server.route('/')
+@server.route("/")
 def index():
-    return "Congratulations, it's a web app!"
-
-
-@server.route('/<b42>')
-def it_be_42(b42):
-    if b42 == "42":
-        return "Congratulations, it's 42!"
+    celsius = request.args.get("celsius", "")
+    if celsius:
+        fahrenheit = fahrenheit_from(celsius)
     else:
-        return "Sorry but your no 42"
+        fahrenheit = ""
+    return (
+        """<form action="" method="get">
+                Celsius temperature: <input type="text" name="celsius">
+                <input type="submit" value="Convert to Fahrenheit">
+            </form>"""
+        + "Fahrenheit: "
+        + fahrenheit
+    )
+
+
+def fahrenheit_from(celsius):
+    """Convert Celsius to Fahrenheit degrees."""
+    try:
+        fahrenheit = float(celsius) * 9 / 5 + 32
+        fahrenheit = round(fahrenheit, 3)  # Round to three decimal places
+        return str(fahrenheit)
+    except ValueError:
+        return "invalid input"
+
+# @server.route('/<int:celsius>')
+# def it_be_42(celsius):
+#     if celsius == 42:
+#         return "Congratulations, it's 42!"
+#     else:
+#         return "Sorry but your no 42"
